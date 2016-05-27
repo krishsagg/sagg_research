a026a77297e5db25008e014b83d75727e6944607
Krishna Gurumurthy <krishna.gurumurthy@SGZ.com>
2016-05-27 15:20:54 +0530

    update

--- Grundfos/src/com/grundfos/generator/dao/JdSimulator.java
+++ Grundfos/src/com/grundfos/generator/dao/JdSimulator.java
@@ -6,7 +6,19 @@ package com.grundfos.generator.dao;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+import org.jsonschema2pojo.AbstractAnnotator;
 import org.jsonschema2pojo.AnnotationStyle;
 import org.jsonschema2pojo.DefaultGenerationConfig;
 import org.jsonschema2pojo.GenerationConfig;
@@ -17,7 +29,16 @@ import org.jsonschema2pojo.SchemaMapper;
 import org.jsonschema2pojo.SchemaStore;
 import org.jsonschema2pojo.rules.RuleFactory;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fasterxml.jackson.annotation.JsonView;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.sun.codemodel.JAnnotationArrayMember;
+import com.sun.codemodel.JAnnotationUse;
 import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JFieldVar;
 
 /**
  * @author krishna.gurumurthy
@@ -41,14 +62,43 @@ public class JdSimulator {
 		}
 		 @Override
 		    public AnnotationStyle getAnnotationStyle() {
-		        return AnnotationStyle.NONE;
+		        return AnnotationStyle.JACKSON2;
 		    }
 		};
+		
+		AbstractAnnotator abstractAnnotr = new Jackson2Annotator() {
+			@Override
+		    public void propertyOrder(JDefinedClass clazz, JsonNode propertiesNode) {
+		        @SuppressWarnings("unused")
+				JAnnotationUse annotationValue = clazz.annotate(Entity.class).param("name",clazz.name());
+		        JAnnotationArrayMember annotationValues = clazz.annotate(NamedQueries.class).paramArray("value");	
+		        Map <String,String> querymap = new HashMap<String,String>();
+		        querymap.put("sample1","select name from Item where dept = 100");
+		        querymap.put("sample2","select name from Item where dept = 100 and clas = 200");
+		        for (Entry<String,String> entry: querymap.entrySet()) {
+		        	JAnnotationUse annotationVal = annotationValues.annotate(NamedQuery.class).param("name",entry.getKey()).param("query", entry.getValue());		        			        	
+		        }
+		    }
+			
+			 @Override
+			    public void propertyField(JFieldVar field, JDefinedClass clazz, String propertyName, JsonNode propertyNode) {
+			        field.annotate(Column.class).param("name", propertyName);
+			        if (field.type().erasure().equals(field.type().owner().ref(Set.class))) {
+			            field.annotate(JsonDeserialize.class).param("as", LinkedHashSet.class);
+			        }
+			        if (propertyNode.has("javaJsonView")) {
+			            field.annotate(JsonView.class).param(
+			                "value", field.type().owner().ref(propertyNode.get("javaJsonView").asText()));
+			        }
+			    }
+		};
+		
+		SchemaMapper mapper = new SchemaMapper(new RuleFactory(config, abstractAnnotr, new SchemaStore()), new SchemaGenerator());
+		mapper.generate(codeModel, "Item", "com.grundfos.generator.entity", source);
 
-		SchemaMapper mapper = new SchemaMapper(new RuleFactory(config, new NoopAnnotator(), new SchemaStore()), new SchemaGenerator());
-		mapper.generate(codeModel, "Sample", "com.example", source);
-
-		codeModel.build(new File("output"));
+		codeModel.build(new File("src"));
+		
+		
 	}
 
 }
